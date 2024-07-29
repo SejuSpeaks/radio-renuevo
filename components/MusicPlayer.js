36fbd41f2504f6ffbe7d062a99071def016b675e
sejuspeaks <elviscodes990@gmail.com>
2024-07-29 19:23:12 -0400

    live radio added

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -2,15 +2,43 @@ import { useState } from "react";
 import { View,Text,Image,StyleSheet, Button,TouchableOpacity,TouchableWithoutFeedback, } from "react-native";
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from "@react-navigation/native";
+import { Audio } from "expo-av";
 
 const MusicPlayer = () => {
 
     const [playing,setPlaying] = useState(false)
+    const [sound, setSound] = useState();
     const nav = useNavigation();
 
-    const changePlay = () => {
-        setPlaying(!playing)
-        console.log(playing)
+
+    const playSound = async (streamUrl) => {
+        console.log('playing sound');
+        const { sound: newSound } = await Audio.Sound.createAsync(
+          { uri: streamUrl },
+          { shouldPlay: true }
+        );
+        setSound(newSound);
+        await newSound.playAsync();
+        setPlaying(true);
+      };
+
+    const changePlay = async () => {
+        if (playing){
+            console.log('stopping sound');
+            setPlaying(false);
+          
+        }
+        else{
+            setPlaying(true);
+            const url = await fetchStreamUrl("https://us2.maindigitalstream.com:2199/tunein/renuevo.pls")
+            console.log(url, 'THIS IS URL');
+            if(url){
+                playSound(url);
+            }
+            else{
+                console.log('failed to stream');
+            }
+        }
     }
 
     return (
@@ -28,8 +56,8 @@ const MusicPlayer = () => {
              
 
                 <View style={{justifyContent:'center'}}>
-                    <Text style={styles.songTitle}>Un nuevo Comienzo</Text>
-                    <Text style={styles.artistName}>Ivelisse Medina</Text>
+                    <Text style={styles.songTitle}>Radio Renuevo</Text>
+                    <Text style={styles.artistName}>102.3FM</Text>
                 </View>
 
             </View>
@@ -44,6 +72,18 @@ const MusicPlayer = () => {
         </View>
         </TouchableOpacity>
     )
+}
+
+const fetchStreamUrl = async (url) => {
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+        const matches = text.match(/File1=(.+)/);
+        return matches ? matches[1].trim() : null;
+    } catch (error) {
+        console.error('Error fetching url', error);
+        return null;
+    }
 }
 
 const styles = StyleSheet.create({
